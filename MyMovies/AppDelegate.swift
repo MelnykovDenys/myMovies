86c31aff12000438d722e47017712a7fb86e0dd6
Denys <melnykow.denis@gmail.com>
2020-12-02 12:37:34 +0100

    create project

---
+++ MyMovies/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  MyMovies
+//
+//  Created by Mac on 02.12.2020.
+//
+
+import UIKit
+import SnapKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+    }
+
+
+}
+
---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Mac on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
